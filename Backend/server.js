99e2074a5b420d5f5419f3b56428082c374d5085
Perwez087 <parwez4568@gmail.com>
2025-10-12 21:50:12 +0530

    update

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-  origin: ["http://localhost:5173", process.env.BASE_URL],
+  origin: ["http://localhost:5173", process.env.CORS_ORIGIN],
   credentials: true,
 }));
 
